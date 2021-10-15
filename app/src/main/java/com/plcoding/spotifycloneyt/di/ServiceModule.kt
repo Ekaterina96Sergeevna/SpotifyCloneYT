948e0350be865875a9c3449e3b08327ac7ab9095
Ekaterina96Sergeevna <ekaterinapavlova_96@mail.ru>
2021-10-15 18:12:15 +0300

    Created dependency injection with Dagger Hilt: AppModule - Glide instance, ServiceModule - some funs for our player

---
+++ app/src/main/java/com/plcoding/spotifycloneyt/di/ServiceModule.kt
@@ -0,0 +1,48 @@
+package com.plcoding.spotifycloneyt.di
+
+import android.content.Context
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.audio.AudioAttributes
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ServiceScoped
+
+@Module
+@InstallIn(ServiceComponent::class) // dependency inside ServiceModule live as long as ServiceComponent does
+object ServiceModule {
+
+
+    @ServiceScoped // we use it only in Service, not in our whole application
+    @Provides
+    // we need call audio attributes which just save some meta information about our player
+    // these don't need any dependencies to be created -  we don't need to pass any parameters
+    fun provideAudioAttributes() = AudioAttributes.Builder()
+        .setContentType(C.CONTENT_TYPE_MUSIC)
+        .setUsage(C.USAGE_MEDIA)
+        .build()
+
+    @ServiceScoped
+    @Provides
+    // player that will play our music
+    fun provideExoPlayer(
+        // some dependencies to be created
+        @ApplicationContext context: Context,
+        audioAttributes: AudioAttributes // we said how create audioAttributes above
+    ) = SimpleExoPlayer.Builder(context).build().apply {
+        setAudioAttributes(audioAttributes, true)
+        setHandleAudioBecomingNoisy(true) // pause our music player for example if user in headphones and so on
+    }
+
+    @ServiceScoped
+    @Provides
+    // source of our data so we will use
+    fun provideDataSourceFactory(
+        @ApplicationContext context: Context
+    ) = DefaultDataSourceFactory(context, Util.getUserAgent(context, "Spotify App"))
+}
